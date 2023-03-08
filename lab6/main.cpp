b9dd4fc486526ea636201eb8a056c551f19eea2b
MeanGold <126110972+MeanGold@users.noreply.github.com>
2023-03-08 17:47:49 -0500

    Update main.cpp

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,8 +1,8 @@
 // My Name: Jonathan Maenche
 // My Class: CPSC 1021 Section 2 Group 4
-// Date: 3/3/2023
+// Date: 3/8/2023
 // Desc: Program that scans in a series of dates and then prints them in ascending order
-// Time: 50m (in lab)
+// Time: 50m (in lab) + 50m (in lab)
 
 #include <iostream>
 #include <iomanip>
@@ -13,27 +13,23 @@
 
 using namespace std; 
 
-//Must define the static member variables at the start of the main.cpp file
-//const std::string Date::MONTHS[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}; 
-
 int main(int argc, char const * argv[]) {
 	int numDates; 
-	string numEntries; 
 	int month, day, year; 
 	
 	ifstream datesFile; 
 	datesFile.open(argv[1]); 
 
-	//getline(datesFile, numEntries); 
+	//Reading in the first line of the file to find number of dates
 	datesFile >> numDates; 
-	//cout << numEntries << endl; 
-	//numDates = stoi(numEntries); 
 
+	//Creating vector of Date objects
 	vector<Date> dates (numDates); 
 	
 	for (int i = 0; i < numDates; i++) {
 		Date sampleDate; 
 
+		//Reading in each value and then assigning it to the 'dates' vector
 		datesFile >> month; 
 		sampleDate.setMonth(month); 
 		datesFile >> day;
@@ -43,8 +39,10 @@ int main(int argc, char const * argv[]) {
 		
 		dates[i] = sampleDate; 
 	}
+	//Algorithm that sorts the 'dates' vector using the 'compare' function
 	sort(&dates[0], &dates[numDates], Date::compare); 
 	
+	//Print out the dates in order
 	for (int i = 0; i < numDates; i++) {
 		cout << dates[i].print() << endl; 
 	}
